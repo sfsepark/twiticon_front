de94e4a68d95cc792ee292c7c11957e1ef0179e1
sfsepark <sfsepark@gmail.com>
2019-12-31 14:53:31 +0900

    모바일 뷰포트

--- pages/_app.js
+++ pages/_app.js
@@ -96,7 +96,7 @@ export default class MyApp extends App{
                     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet"/>
                     <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon"/>
                     <link rel="shortcut icon" href="http://twiticon.com/favicon.ico?v=2" type="image/x-icon"/>
-
+                    <meta name="viewport" content="width=device-width, initial-scale=0.65"/>
                 </Head>
 
                 <Navbar 
