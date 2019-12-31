d86ffe0d9fcb6cf6f7a708c0601eb6b7506344cb
sfsepark <sfsepark@gmail.com>
2019-12-31 22:06:19 +0900

    프로덕트버전에 테스트 페이지 삭제

--- pages/test.js
+++
@@ -1,7 +0,0 @@
-import ChatBox from '../component/chatBox'
-
-export default (props) => {
-
-    //console.log(props.)
-    return <ChatBox width = {props.width ? props.width : 500} height = {props.height ? props.height : 500}/>;
-}
